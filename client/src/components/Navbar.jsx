0510fe248490b9a6a3f18801f35ee593fa07e851
Bilal05476 <ba4762313@gmail.com>
2022-02-24 20:07:59 +0500

    updates

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -38,13 +38,19 @@ const Navbar = () => {
           />
         )}
         {toggleMenu && (
-          <ul className="z-10 fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none flex flex-col
-          justify-start item-end rounded-md blue-glass text-white animate-slide-in"> 
-            <li className="text-xl w-full my-2">
+          <ul
+            className="z-10 fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none flex flex-col
+          justify-start item-end rounded-md blue-glassmorphism text-white animate-slide-in"
+          >
+            <li className="cursor-pointer text-xl w-full my-2">
               <AiOutlineClose onClick={() => setToggleMenu(false)} />
             </li>
             {["Market", "Exchange", "Tutorials", "Wallets"].map((item, ind) => (
-              <NavbarItem key={item + ind} title={item} classProps="my-2 text-lg" />
+              <NavbarItem
+                key={item + ind}
+                title={item}
+                classProps="my-2 text-lg"
+              />
             ))}
           </ul>
         )}
