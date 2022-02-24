b30d2b48ab9644d4842a09042eb9b41f0c8b2320
Bilal05476 <ba4762313@gmail.com>
2022-02-24 19:55:29 +0500

    updates

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const NavbarItem = ({ title, classProps }) => {
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
   return (
-    <nav className="w-full flex-md:justify-center justify-between items-center p-4">
+    <nav className="w-full flex md:justify-center justify-between items-center p-4">
       <div className="md:flex-[0.5] flex-initial justify-center items-center">
         <img src={logo} alt="logo" className="w-32 cursor-pointer" />
       </div>
@@ -38,8 +38,8 @@ const Navbar = () => {
           />
         )}
         {toggleMenu && (
-          <ul className="z-10 fixed top-0 right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none flex flex-col
-          justify-start item-end rounded">
+          <ul className="z-10 fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none flex flex-col
+          justify-start item-end rounded-md blue-glass text-white animate-slide-in"> 
             <li className="text-xl w-full my-2">
               <AiOutlineClose onClick={() => setToggleMenu(false)} />
             </li>
