6571ad2ef67b45d6b3249b284a2bf1d15e06d409
Bilal05476 <ba4762313@gmail.com>
2022-02-24 19:41:39 +0500

    updates

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,56 @@
 import { HiMenuAlt4 } from "react-icons/hi";
-import {AiOutlineClose} from "react-icons/ai";
+import { AiOutlineClose } from "react-icons/ai";
+
+import logo from "../../images/logo.png";
+import { useState } from "react";
+
+const NavbarItem = ({ title, classProps }) => {
+  return <li className={`mx-4 cursor-pointer ${classProps}`}>{title}</li>;
+};
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [toggleMenu, setToggleMenu] = useState(false);
+  return (
+    <nav className="w-full flex-md:justify-center justify-between items-center p-4">
+      <div className="md:flex-[0.5] flex-initial justify-center items-center">
+        <img src={logo} alt="logo" className="w-32 cursor-pointer" />
+      </div>
+      <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
+        {["Market", "Exchange", "Tutorials", "Wallets"].map((item, ind) => (
+          <NavbarItem key={item + ind} title={item} />
+        ))}
+        <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
+          Login
+        </li>
+      </ul>
+      <div className="flex relative">
+        {toggleMenu ? (
+          <AiOutlineClose
+            fontSize={28}
+            className="text-white cursor-pointer md:hidden"
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <HiMenuAlt4
+            fontSize={28}
+            className="text-white cursor-pointer md:hidden"
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+        {toggleMenu && (
+          <ul className="z-10 fixed top-0 right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none flex flex-col
+          justify-start item-end rounded">
+            <li className="text-xl w-full my-2">
+              <AiOutlineClose onClick={() => setToggleMenu(false)} />
+            </li>
+            {["Market", "Exchange", "Tutorials", "Wallets"].map((item, ind) => (
+              <NavbarItem key={item + ind} title={item} classProps="my-2 text-lg" />
+            ))}
+          </ul>
+        )}
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
